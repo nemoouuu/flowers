2d1e887d1707e17e93b7035588ed12d478177270
nemoouuu_ <owen.marckwardt@gmail.com>
2025-10-08 18:50:58 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U KEZIA 💝').split('')
+    const titles = ('I LOVE U GYTA 💝').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
